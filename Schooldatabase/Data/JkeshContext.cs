946d646288e4deb201fab56330ddcb9a65493afc
Jambukesh <jkesh360@gmail.com>
2024-01-03 12:29:57 +0200

    Phase2-project4

---
+++ Schooldatabase/Data/JkeshContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Schooldatabase.Models;
+
+namespace Schooldatabase.Data
+{
+    public class JkeshContext : DbContext
+    {
+        public JkeshContext (DbContextOptions<JkeshContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Schooldatabase.Models.Student> Student { get; set; } = default!;
+    }
+}
